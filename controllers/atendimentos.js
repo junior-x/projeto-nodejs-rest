4571b9159082fc1a43da5e1834273200acfc37cb
junior-x <marcosmansur@outlook.com>
2022-02-18 11:46:10 -0800

    raw

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,9 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Voce esta em atendimentos'));
+
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        
+        res.send('Voce esta realizando um POST')
+    })
 }
